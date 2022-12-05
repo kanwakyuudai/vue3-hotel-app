2c58341e8190cda5b3a847c0554d3dbc9e976a8e
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-05 14:26:31 +0800

    导入路由。

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 导入路由
+import router from './router'
 // 重置默认 CSS 样式
 import 'normalize.css'
 import './assets/css/index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
