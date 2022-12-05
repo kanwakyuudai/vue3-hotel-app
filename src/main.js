22d1d7267747a407ee5e73c4dc11b71821f5c524
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-05 14:18:34 +0800

    重置默认 CSS 样式。

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import './assets/main.css'
+// 重置默认 CSS 样式
+import 'normalize.css'
+import './assets/css/index.css'
 
 createApp(App).mount('#app')
