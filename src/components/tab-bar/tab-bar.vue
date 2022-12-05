846d7765ec5fc27df71777759775069a19817b32
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-05 22:12:42 +0800

    使用组件库重构底栏，无需手动配置路由。

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="tab-bar">
-
-    <!-- 循环渲染四个底栏导航按钮 -->
-    <template v-for="(item, index) in tabBarData">
-      <div
-      class="tab-bar-item"
-      :class="{active: currentIndex === index}"
-      @click="itemClick(item, index)"
-      >
-        <img v-if='(currentIndex !== index)' :src="getAssetsURL(item.image)" alt="">
-        <img v-else :src="getAssetsURL(item.imageActive)" alt="">
-        <span class="text">{{ item.text }}</span>
-      </div>
-    </template>
-
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabBarData">
+        <!-- 使用组件库自动路由 -->
+        <van-tabbar-item :to="item.path">
+          <span class="text">{{ item.text }}</span>
+          <template #icon>
+            <!-- 使用插槽自定义图标 -->
+            <img v-if='(currentIndex !== index)' :src="getAssetsURL(item.image)" alt="">
+            <img v-else :src="getAssetsURL(item.imageActive)" alt="">
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
@@ -23,46 +22,16 @@ import tabBarData from '@/assets/data/tab-bar'
 import { getAssetsURL } from '@/utils/load_assets'
 
 import { ref } from "vue";
-import { useRouter } from "vue-router";
 
+// 响应式记录当前点击按钮
 const currentIndex = ref(0)
-const router = useRouter()
-
-const itemClick = (item, index) => {
-  currentIndex.value = index
-  router.push(item.path)
-}
 </script>
 
 <style lang="scss" scoped>
 .tab-bar {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  display: flex;
-
-  border-top: 1px solid #f3f3f3;
-
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    &.active {
-      color: $themeColor;
-    }
-    .text {
-      font-size: 12px;
-      margin-top: 2px;
-    }
-
-    img {
-      width: 36px;
-    }
+  img {
+    // 修改组件库默认图标大小
+    height: 28px;
   }
 }
 </style>
