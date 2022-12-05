2d63bf9d86641a2028ed428c3252d9890c67d9c8
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-05 19:26:22 +0800

    配置 Pinia 状态管理。

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
 <template>
   <div class="app">
     <router-view></router-view>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/favorite">收藏</router-link>
+    <router-link to="/order">订单</router-link>
+    <router-link to="/message">消息</router-link>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// 导入路由
-import router from './router'
 // 重置默认 CSS 样式
 import 'normalize.css'
 import './assets/css/index.css'
+// 导入 Pinia 状态管理
+import pinia from './stores'
+// 导入路由
+import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
