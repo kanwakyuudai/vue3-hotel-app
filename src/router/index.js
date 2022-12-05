dc985227401cae9798eaa4bf134f36494bf06ecb
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-05 17:58:30 +0800

    配置路由使用 # 跳转。

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,29 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: []
+  routes: [
+    { // 默认跳转首页
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: () => import ('@/views/home/home.vue')
+      // 懒加载
+    },
+    {
+      path: '/favorite',
+      component: () => import ('@/views/favorite/favorite.vue')
+    },
+    {
+      path: '/message',
+      component: () => import ('@/views/message/message.vue')
+    },
+    {
+      path: '/order',
+      component: () => import ('@/views/order/order.vue')
+    }
+  ]
 })
 
 export default router
---
+++ src/views/favorite/favorite.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="favorite">
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
