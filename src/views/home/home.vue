75dc942946fe478e05b239c3ef85357d4826ed1c
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-05 22:42:46 +0800

    抽取首页横幅为单一组件。

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home">
+    <home-nav-bar></home-nav-bar>
+    <div class="banner">
+      <img src="@/assets/imgs/home/banner.webp" alt="">
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import HomeNavBar from './subs/home-nav-bar.vue';
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+  img {
+    width: 100%;
+  }
+}
 </style>
---
+++ src/views/home/subs/home-nav-bar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nav-bar">
+    <div class="title">趣旅行住民宿</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.nav-bar{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 46px;
+  border-bottom: 1px solid #f2f2f2;
+
+  .title {
+    color: $themeColor;
+    font-size: 18px;
+    font-weight: 700;
+  }
+}
+</style>
