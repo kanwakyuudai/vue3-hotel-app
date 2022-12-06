5d9ef42d149ba7b2eb7b1c886c1bb3c37189f3f2
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-06 18:19:41 +0800

    实现城市列表顶部搜索栏和国内外切换标签。

--- src/views/cities/cities.vue
+++ src/views/cities/cities.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-
+  <div class="cities-list">
+    <!-- 顶部搜索条 -->
+    <van-search
+      shape="round"
+      show-action
+      @cancel="cancelSearch"
+      v-model="searchKeyWord"
+      placeholder="城市/区域/位置"
+    />
+    <!-- 国内海外城市标签切换 -->
+    <van-tabs v-model:active="switchTab">
+      <van-tab title="国内·港澳台"></van-tab>
+      <van-tab title="海外"></van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter()
 
+// 搜索框组件功能
+const searchKeyWord =ref('')
+const cancelSearch = () => {
+  router.back()
+}
+
+// 国内海外城市切换
+const switchTab = ref() // 默认绑定标签索引
 </script>
 
 <style lang="scss" scoped>
