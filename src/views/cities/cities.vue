55593432b6d29ba15eccd1c6847528053a4b9eb9
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-06 20:46:02 +0800

    计算属性获取响应式访问数据。

--- src/views/cities/cities.vue
+++ src/views/cities/cities.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="cities-list">
-    <!-- 顶部搜索条 -->
-    <van-search
-      shape="round"
-      show-action
-      @cancel="cancelSearch"
-      v-model="searchKeyWord"
-      placeholder="城市/区域/位置"
-    />
-    <!-- 国内海外城市标签切换 -->
-    <van-tabs v-model:active="switchTab">
-      <template v-for="(value, key, index) in allCities" :key="key">
-        <van-tab :title="value.title">
-        </van-tab>
-      </template>
-    </van-tabs>
+    <div class="top">
+      <!-- 顶部搜索条 -->
+      <van-search
+        shape="round"
+        show-action
+        @cancel="cancelSearch"
+        v-model="searchKeyWord"
+        placeholder="城市/区域/位置"
+      />
+      <!-- 国内海外城市标签切换 -->
+      <van-tabs v-model:active="tabActive">
+        <template v-for="(value, key, index) in allCities" :key="key">
+          <van-tab :title="value.title" :name="key">
+          <!-- :name 变更绑定到 key 默认值是 index -->
+          </van-tab>
+        </template>
+      </van-tabs>
+    </div>
+    <div class="content">
+      <template v-for="cityGroup in currentGroup?.cities"><div>{{cityGroup}}</div></template>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
-// import { getCitiesList } from '@/services';
 import useCityStore from '@/stores/modules/city'
 import { storeToRefs } from 'pinia';
 const router = useRouter()
@@ -34,19 +39,7 @@ const cancelSearch = () => {
 }
 
 // 国内海外城市切换
-const switchTab = ref() // 默认绑定标签索引
-
-// 原生 Axios 请求数据的写法：
-// axios.get('url').then(res => {})
-
-// 发起网络请求，拉取数据
-// const allCities = ref({})
-// getCitiesList().then(res => {
-//   allCities.value = res.data
-// })
-// 目前实现的两个缺点：
-//   一，组件内充斥着请求逻辑；
-//   二，需要手动给子组件（若存在）传数据。（将使用 Pinia 改善）
+const tabActive = ref() // 默认绑定 index，通过组件库 :name 指定绑定 key
 
 // 从 Pinia 中获取数据
 const cityStore = useCityStore()
@@ -54,8 +47,17 @@ const cityStore = useCityStore()
 cityStore.fetchAllCitiesData()
 // 使取得的数据变成响应式
 const { allCities } = storeToRefs(cityStore)
+
+// 只要是 ref 取其值都需要 .value ，但是取到的值丢失响应式，因此使用计算属性
+const currentGroup = computed(() => allCities.value[tabActive.value])
 </script>
 
 <style lang="scss" scoped>
-
+.cities-list {
+  .content {
+    // 为顶部搜索条留出空间
+    height: calc(100vh - 98px);
+    overflow-y: auto;
+  }
+}
 </style>
