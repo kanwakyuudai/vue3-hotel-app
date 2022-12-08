3ca251a661a753675c7456593122534daeb67fd9
kanwakyuudai <kanwakyuudai@pm.me>
2022-12-08 22:38:28 +0800

    实现顶部搜索条骨架，固定定位，下滑显示。

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -7,20 +7,24 @@
     <home-search-box></home-search-box>
     <home-categories></home-categories>
     <home-content></home-content>
+    <div class="search-bar" v-if="isShowSearchBar">
+      <search-bar></search-bar>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { watch } from 'vue';
+import { ref, watch, computed } from 'vue';
 import HomeNavBar from './subs/home-nav-bar.vue';
 import HomeSearchBox from './subs/home-search-box.vue';
 import HomeCategories from './subs/home-categories.vue';
 import HomeContent from './subs/home-content.vue';
+import SearchBar from '@/components/search-bar/search-bar.vue';
 import useHomeStore from '@/stores/modules/home';
-import whenScrollToBottom from '@/hooks/whenScroll'
+import useScroll from '@/hooks/whenScroll'
 
 const homeStore = useHomeStore()
-const {isReachBottom} = whenScrollToBottom()
+const { isReachBottom, clientHeight, scrollTop, scrollHeight } = useScroll()
 
 watch(isReachBottom, (newValue) => {
   if (newValue) {
@@ -30,6 +34,19 @@ watch(isReachBottom, (newValue) => {
     })
   }
 })
+// 下滑一会儿后响应式显示搜索条
+
+// const isShowSearchBar = ref(false)
+// watch(scrollTop, (newTop) => {
+//   isShowSearchBar.value = newTop > 100
+// })
+
+// 使用计算属性重写上面的逻辑
+// 定义一个新响应式变量，若依赖之前的响应式变量，应使用计算属性
+
+const isShowSearchBar = computed(() => {
+  return scrollTop.value >= 350
+})
 </script>
 
 <style lang="scss" scoped>
@@ -37,5 +54,16 @@ watch(isReachBottom, (newValue) => {
   img {
     width: 100%;
   }
+}
+
+.search-bar {
+  position: fixed;
+  z-index: 20;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 45px;
+  padding: 16px 16px 10px;
+  background-color: #fff;
 }
 </style>
